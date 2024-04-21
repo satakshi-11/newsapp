3eaf46de5ab93d158acca14e985187d018c37fdf
Km Satakshi Singh <105787853+satakshi-11@users.noreply.github.com>
2024-04-21 12:21:26 +0530

    adding previous and next buttons

--- src/Components/News.js
+++ src/Components/News.js
@@ -3,20 +3,42 @@ import NewsItem from './NewsItem'
 
 export class News extends Component {
   
+  handlenext=async()=>{
+    if(Math.ceil(this.state.page+1>this.state.totalResults/20)){
+
+    }
+    else{
+    let url= `https://newsapi.org/v2/top-headlines?country=in&apiKey=e50468325df74c05bf409738298163cb&page=${this.state.page+1}&pageSize=20`;
+    let data= await fetch(url);
+    let parsedData=await data.json();
+    this.setState({    
+      articles:parsedData.articles,
+      page:this.state.page+1
+    
+    })
+  }
+  }
+  handleprev=async()=>{
+    let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=e50468325df74c05bf409738298163cb&page=${this.state.page-1}&pageSize=20`;
+    let data= await fetch(url);
+    let parsedData=await data.json();
+    this.setState({articles:parsedData.articles,page:this.state.page-1})
+  }
   constructor (){
     super();
-    console.log("Hello I am constructor from news ")
     this.state={
     articles:[],
-    loading:false
+    loading:false,
+    page:1
     }
   }
    async componentDidMount(){
     let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=e50468325df74c05bf409738298163cb";
     let data= await fetch(url);
-    let parseData=await data.json();
-    this.setState({articles:parseData.articles})
+    let parsedData=await data.json();
+    this.setState({articles:parsedData.articles,totalResults:parsedData.totalResults})
   }
+
   render() {
     return (
       <div className='container my-3' >
@@ -30,11 +52,14 @@ export class News extends Component {
         <NewsItem  title={element.title} description={element.description} imageUrl={element.urlToImage} url={element.url} />
         </div>
         })}
-       
-         
-     
+           
       </div>
+      <div className="container d-flex justify-content-between ">
+      <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handleprev} >&larr; Previous</button>
+ <button type="button" className="btn btn-dark" onClick={this.handlenext}>Next &rarr;</button>
+  </div>
       </div>
+   
     )
   }
 }
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -12,10 +12,11 @@ export class NewsItem extends Component {
      <div className="card-body">
      <h5 className="card-title">{title}</h5>
      <p className="card-text">{description}</p>
-     <a href={url} target='_blank' class="btn btn-sm btn-primary">Read More</a>
+     <a rel='noreferrer' href={url} target='_blank' class="btn btn-sm btn-dark">Read More</a>
      </div>
 </div>
       </div>
+     
     )
   }
 }
